ff1c1bb17ecf96197566a3adbc84ecb7816cf862
Praveen31Ojha <ojha.praveenk@gmail.com>
2025-05-27 01:17:08 +0200

    Updated src link and trimmed info

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Section from './Components/Section';
 import Contact from './Components/Contact';
-import Credit from "./Components/Credit";
 import Education from "./Components/Education";
 import Testimonials from "./Components/Testimonials";
 import './App.css';
-// import contactImg from '../src/assets/section.jpg'
-import contactImg from '../src/assets/profile_photo.jpg'
-import SkillsTrain from "./Components/SkillsTrain";
 import Projects from "./Components/ProjectsPage";
 function App() {
-  // const location = useLocation();
-  // const params = new URLSearchParams(location.search);
-  // const mode = params.get('mode') || 'data-analyst'; // default as you prefer
   return (
     <Router>
       {/* Navbar is always displayed */}
@@ -31,9 +23,6 @@ function App() {
         
         {/* Add more routes as needed */}
       </Routes>
-      {/* <SkillsTrain/>
-
-      <Credit /> */}
     </Router>
   );
 }
