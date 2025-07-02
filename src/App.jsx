4e3f17998d80bbcf2477fce6a3759e1873e38abb
Praveen31Ojha <ojha.praveenk@gmail.com>
2025-07-02 23:12:21 +0200

    Removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from './Components/Navbar';
-// import Section from './Components/Section';
-// import Contact from './Components/Contact';
-// import Education from "./Components/Education";
-// import Testimonials from "./Components/Testimonials";
-// import Projects from "./Components/ProjectsPage";
-// function App() {
-//   return (
-//     <Router>
-//       {/* Navbar is always displayed */}
-//       <Navbar />
-
-//       {/* Main content area */}
-//       <Routes>
-//         <Route path="/" element={<Section/>}/>
-//         <Route path="/section" element={<Section/>}/>
-//         <Route path="/education" element={<Education />} />
-//         <Route path="/projects" element={<Projects />} />
-//         <Route path="/testimonials" element={<Testimonials />} />
-//         <Route path="/contact" element={<Contact/>} />
-        
-//         {/* Add more routes as needed */}
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import React, { useRef, useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
